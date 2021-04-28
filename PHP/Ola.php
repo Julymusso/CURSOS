db65ce7bb8435362bd1551642c4257df31e48507
Julymusso <julianamusso@hotmail.com>
2021-04-28 20:59:05 -0300

    Initial commit

---
+++ PHP/Ola.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8"/>
+        <title> Primeiro exemplo PHP </title>
+    </head>
+    <style>
+    h2{
+        color: #00d100ff;
+    }
+    </style>
+    <body>
+        <h1>Testando PHP</h1>
+        <?php
+            echo "<h2> Olá <br>Mundo</br> </h2>";
+        ?>
+    </body>
+</html>
